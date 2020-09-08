5c08a6705e6d56a526e0c60dd599e9e017c81412
RamyaSabapathi2018 <mailramya26@gmail.com>
2020-09-08 22:32:26 +0530

    Added to sample2

--- src/test/java/org/GIT/PushCode.java
+++ src/test/java/org/GIT/PushCode.java
@@ -3,6 +3,10 @@ package org.GIT;
 public class PushCode {
 	public static void main(String[] args) {
 		int a=10;
+		System.out.println("Conflict");
+		System.out.println("Conflict");
+		System.out.println("Conflict");
+		System.out.println("Conflict");
 		System.out.println("Sum"+(a+10));
 	}
 
