af361af2b02d1603ed300b5e12c1b37a2710bb95
RamyaSabapathi2018 <mailramya26@gmail.com>
2020-09-08 22:27:22 +0530

    Added to Sample1

--- src/test/java/org/GIT/PushCode.java
+++ src/test/java/org/GIT/PushCode.java
@@ -3,7 +3,9 @@ package org.GIT;
 public class PushCode {
 	public static void main(String[] args) {
 		int a=10;
+		String s= "ffffff";
 		System.out.println("Sum"+(a+10));
+		System.out.println(s.length());
 	}
 
 }
